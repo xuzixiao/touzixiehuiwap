774347a69c7ec53657b7beae3baf70902c889662
xuzixiao <644577759@qq.com>
2017-07-19 19:35:11 +0800

    唐人街跨境电商手机端‘“

---
+++ js/wap.js
@@ -0,0 +1,41 @@
+function opensc(){
+	$(".zhezhao").fadeIn();
+	$("#shoucang").fadeIn(100);
+	$("body").addClass("unused");
+}
+function closesc(){
+	$(".zhezhao").fadeOut();
+	$("#shoucang").fadeOut(100);
+	$("body").removeClass("unused");
+}
+function closezz(){
+	$(".zhezhao").fadeOut();
+	$("#shoucang").fadeOut(100);
+	$("body").removeClass("unused");
+}
+function openfx(){
+	$(".zhezhao").fadeIn();
+	$("#fenxiang").fadeIn(100);
+	$("body").addClass("unused");
+}
+function closefx(){
+	$(".zhezhao").fadeOut();
+	$("#fenxiang").fadeOut(100);
+	$("body").removeClass("unused");
+}
+function opensc(){
+	$(".zhezhao").fadeIn();
+	$("#shoucang").fadeIn(100);
+	$("body").addClass("unused");
+}
+function closesc(){
+	$(".zhezhao").fadeOut();
+	$("#shoucang").fadeOut(100);
+	$("body").removeClass("unused");
+}
+function closezz(){
+	$(".zhezhao").fadeOut();
+	$(".fenxiang-box").fadeOut(100);	
+	$("#shoucang").fadeOut(100);
+	$("body").removeClass("unused");
+}
